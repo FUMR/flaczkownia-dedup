b97023c574ccd2b25db1f95c7c57c370e197ec6b
Piotr Łoboda <authentik@lobson.pl>
2025-10-04 21:08:49 +0200

    feat: a script filling up the queue with already existing data

---
+++ fill_up_queue.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR="$1"
+URL="$2"
+
+[[ -d "$DIR" ]] || { echo "Usage: $0 <directory_path> <url>"; exit 1; }
+
+declare -A lookup
+
+for f in *; do
+    filename="$(basename "$f")"
+    msg_id="${filename%% *}"
+    [[ "$msg_id" =~ ^[0-9]+$ ]] || continue
+    lookup[$msg_id]="$f"
+done
+
+for id in "${!lookup[@]}"; do
+    echo "$id"
+done | sort -nr | while read -r id; do
+    json='{
+      "msg_id": '"$id"',
+      "chat_id": 0,
+      "sender_id": 0,
+      "fname": '"${lookup[$id]}"'
+      "mimetype": x
+      "voice": true
+      "video": true
+    }'
+
+    curl -s -X POST -H "Content-Type: application/json" -d "$json" "$URL"
+done
