6cfe6c1fbc65512ca1ddd6de2436ed39c095754f
Piotr Łoboda <authentik@lobson.pl>
2025-10-07 19:38:10 +0200

    feat(dedup): add connector notification about a new file

--- dedup.py
+++ dedup.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+import httpx
 from time import sleep
 
 import audioprint
@@ -38,14 +39,15 @@ def _audioprint_resampled(file_path):
     return audioprint.audio_phash(raw_pcm_data, 44100)
 
 
-def process_path(path, session):
+def process_path(path, db_session, http_session):
     logger.info(f"Processing path: {path}")
 
     for file in _recursive_path_walk(path):
         logger.info(f"Processing file: {file}")
 
         # Skip already indexed
-        if session.query(Track).filter_by(path=file).first() or session.query(UnknownFile).filter_by(path=file).first():
+        if db_session.query(Track).filter_by(path=file).first() or db_session.query(UnknownFile).filter_by(
+                path=file).first():
             logger.info(f"Skipping already indexed file: {file}")
             continue
 
@@ -53,15 +55,15 @@ def process_path(path, session):
             mf = MediaFile(file)
         except mediafile.FileTypeError:
             uf = UnknownFile(path=file)
-            session.add(uf)
-            session.commit()
+            db_session.add(uf)
+            db_session.commit()
             logger.info(f"Skipping file in unsupported format: {file}")
             continue
 
         fp = _audioprint_resampled(file)
 
         # Check if the same track already exists
-        existing = session.query(Track).filter_by(
+        existing = db_session.query(Track).filter_by(
             acoustic_fingerprint=fp,
             album=mf.album,
             mb_albumid=mf.mb_albumid,
@@ -79,8 +81,10 @@ def process_path(path, session):
             duplicate=existing is not None,
         )
 
-        session.add(track)
-        session.commit()
+        db_session.add(track)
+        db_session.commit()
+        if existing is None:
+            http_session.post(path=f"dedup_webhook", json={"fname": file})
 
         logger.info(f"Processed file: {file}, duplicate={existing is not None}")
 
@@ -90,21 +94,27 @@ def main():
     parser.add_argument("--directory", help="Path to flaczkownia directory. Starts in queue mode if not provided.")
     parser.add_argument("--db", default="sqlite:///data/dedup.sqlite3",
                         help="Database URL (eg. sqlite or pgsql path)")
+    parser.add_argument("--connector", default="http://connector:8000",
+                        help="Connector URL")
     args = parser.parse_args()
 
     logger.info("Initializing database")
     engine = create_engine(args.db, echo=False)
     SQLBase.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+    db_session = sessionmaker(bind=engine)()
     logger.info("Database initialized")
 
+    logger.info("Initializing connector session")
+    http_session = httpx.Client(base_url=args.connector)
+    logger.info("Connector session initialized")
+
     if args.directory:
-        process_path(args.directory, session)
+        process_path(args.directory, db_session, http_session)
         return
 
     while True:
         try:
-            job: Queue | None = session.query(Queue).filter_by(status=JobStatus.PENDING).order_by(
+            job: Queue | None = db_session.query(Queue).filter_by(status=JobStatus.PENDING).order_by(
                 Queue.created_at).first()
 
             if job is None:
@@ -112,16 +122,16 @@ def main():
                 continue
 
             logger.info(f"Starting processing of job with queue_id: {job.id}")
-            job.update_status(JobStatus.PROCESSING, session)
+            job.update_status(JobStatus.PROCESSING, db_session)
 
             try:
-                process_path(job.path, session)
+                process_path(job.path, db_session, http_session)
             except Exception as e:
-                job.update_status(JobStatus.FAILED, session)
+                job.update_status(JobStatus.FAILED, db_session)
                 logger.exception("Job failed")
                 continue
 
-            job.update_status(JobStatus.DONE, session)
+            job.update_status(JobStatus.DONE, db_session)
             logger.info("Job done")
         except KeyboardInterrupt:
             break
